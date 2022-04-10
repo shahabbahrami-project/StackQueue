c7b7bbdcfa17ca9ce002a24bc4494b8d7ece02f8
Shahab Bahrami <bahramis@ece.ubc.ca>
2022-04-09 20:09:42 -0700

    Creat Queue class with print stack method

--- main.py
+++ main.py
@@ -13,6 +13,62 @@ class Stack:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        return True
 
+    def pop(self):
+        if self.height == 0:
+            return None
+        else:
+            temp = self.top
+            self.top = temp.next
+            temp.next = None
+            self.height -= 1
+        return temp.value
 myStack = Stack(4)
+myStack.push(33)
+myStack.pop()
 myStack.print_stack()
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last =new_node
+        else:
+            new_node.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return True
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            temp = self.first
+            self.first = temp.next
+            temp.next = None
+        self.length -= 1
+        return temp.value
